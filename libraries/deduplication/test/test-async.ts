7b316c477e7d714b470c0fd06cdd1d1d6464813e
Garrett Serack <garretts@microsoft.com>
2018-12-03 13:31:44 -0800

    extend test timeout

--- libraries/deduplication/test/test-async.ts
+++ libraries/deduplication/test/test-async.ts
@@ -8,11 +8,11 @@ import * as aio from "@microsoft.azure/async-io";
 
   @test async "components and paths deduplication"() {
 
-    const input = JSON.parse(await aio.readFile(`${__dirname}../../../test/resources/input.yaml`));
-    const expectedOutput = JSON.parse(await aio.readFile(`${__dirname}../../../test/resources/output.yaml`));
+    const input = JSON.parse(await aio.readFile(`${__dirname}/../../../test/resources/input.yaml`));
+    const expectedOutput = JSON.parse(await aio.readFile(`${__dirname}/../../../test/resources/output.yaml`));
 
     // TODO: test mappings.
     const deduplicator = new Deduplicator(input);
     assert.deepStrictEqual(deduplicator.output, expectedOutput);
   }
-} 
+}
