4dc1d4cf733ed9ae2cc46c4dea5b20edb8eda712
Nelson Daniel Troncoso <nd.ta@hotmail.com>
2019-04-09 17:23:33 -0700

    clean

--- libraries/codegen/test/test-async.ts
+++ libraries/codegen/test/test-async.ts
@@ -1,11 +1,10 @@
-import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
-import * as assert from "assert";
-import { toSemver } from "../apiversion";
-
+import * as assert from 'assert';
+import { only, skip, slow, suite, test, timeout } from 'mocha-typescript';
+import { toSemver } from '../apiversion';
 
 @suite class ApiVersion {
 
-  @test async "to semver conversion"() {
+  @test async 'to semver conversion'() {
     const actual1 = toSemver('6.2.0.9');
     const expected1 = '6.2.0';
     assert.strictEqual(actual1, expected1);
@@ -14,16 +13,13 @@ import { toSemver } from "../apiversion";
     const expected2 = '2017.10.12';
     assert.strictEqual(actual2, expected2);
 
-
     const actual3 = toSemver('2017-10-12-preview');
     const expected3 = '2017.10.12-preview';
     assert.strictEqual(actual3, expected3);
 
-
     const actual4 = toSemver('v2.1');
     const expected4 = '2.1.0';
     assert.strictEqual(actual4, expected4);
-
 
     const actual5 = toSemver('1.1');
     const expected5 = '1.1.0';
@@ -35,6 +31,6 @@ import { toSemver } from "../apiversion";
 
     const actual7 = toSemver('1.3.1');
     const expected7 = '1.3.1';
-    assert.strictEqual(actual6, expected6);
+    assert.strictEqual(actual7, expected7);
   }
 }
