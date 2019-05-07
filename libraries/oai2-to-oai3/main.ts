9b178f1f22a2e36228e5d1b97d6bf72856dce464
Nelson Daniel Troncoso <nd.ta@hotmail.com>
2019-05-07 10:42:12 -0700

    oai2-to-oai3: bug fix where some request-body extensions got lost

--- libraries/oai2-to-oai3/main.ts
+++ libraries/oai2-to-oai3/main.ts
@@ -84,7 +84,7 @@ export class Oai2ToOai3 {
       if (this.generated.servers === undefined) {
         this.generated.servers = this.newArray('/basePath');
       }
-      this.generated.servers.__push__(server);
+      this.generated.servers.__push__({ value: server, pointer: '/basePath' });
     }
 
     if (this.generated.servers === undefined) {
@@ -893,6 +893,13 @@ export class Oai2ToOai3 {
           if (parameterValue.type === 'array' && parameterValue.items !== undefined) {
             targetProperty.items = { value: parameterValue.items, pointer };
           }
+
+          // copy extensions in target property
+          for (const { key, pointer: fieldPointer, value } of parameterItemMembers()) {
+            if (key.startsWith('x-')) {
+              targetProperty[key] = { value: value, pointer: fieldPointer };
+            }
+          }
         }
       } else if (parameterValue.type === 'file') {
 
@@ -900,7 +907,6 @@ export class Oai2ToOai3 {
         targetOperation['application/octet-stream'].schema = this.newObject(pointer);
         targetOperation['application/octet-stream'].schema.type = { value: 'string', pointer };
         targetOperation['application/octet-stream'].schema.format = { value: 'binary', pointer };
-
       }
 
       if (parameterValue.in === 'body') {
@@ -929,6 +935,13 @@ export class Oai2ToOai3 {
             targetOperation.requestBody.content[mimetype].schema = this.newObject(pointer);
           }
 
+        }
+
+        // copy extensions in requestBody
+        for (const { key, pointer: fieldPointer, value } of parameterItemMembers()) {
+          if (key.startsWith('x-')) {
+            targetOperation.requestBody[key] = { value: value, pointer: fieldPointer };
+          }
         }
       }
     } else {
