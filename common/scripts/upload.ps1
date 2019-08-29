81db5b005878845db0db703c9cd03b99b25aaee9
Garrett Serack <garretts@microsoft.com>
2019-08-28 17:01:32 -0700

    fix publish script

--- common/scripts/upload.ps1
+++ common/scripts/upload.ps1
@@ -1,5 +1,7 @@
 param( $npmauthtoken ) 
+set-content -Value "//registry.npmjs.org/:_authToken=$npmauthtoken" -path ./.npmrc 
+$ErrorActionPreference = 'silentlycontinue'
 
-dir $PSScriptRoot/../temp/artifacts/packages/*.tgz |% { 
-  & "$PSScriptRoot/../temp/pnpm-local/node_modules/.bin/pnpm" publish $_ --tag preview --scope public "--$npmauthtoken" 
+dir $PSScriptRoot/../temp/artifacts/packages/*.tgz | % { 
+  & "$PSScriptRoot/../temp/pnpm-local/node_modules/.bin/pnpm" publish $_ --tag preview --access public 
 }
