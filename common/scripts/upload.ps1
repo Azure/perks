04efce77ea18b071e1279ff908b54d3650279d77
Garrett Serack <garretts@microsoft.com>
2019-08-28 17:03:54 -0700

    make publish just one script

--- common/scripts/upload.ps1
+++
@@ -1,7 +0,0 @@
-param( $npmauthtoken ) 
-set-content -Value "//registry.npmjs.org/:_authToken=$npmauthtoken" -path ./.npmrc 
-$ErrorActionPreference = 'silentlycontinue'
-
-dir $PSScriptRoot/../temp/artifacts/packages/*.tgz | % { 
-  & "$PSScriptRoot/../temp/pnpm-local/node_modules/.bin/pnpm" publish $_ --tag preview --access public 
-}
