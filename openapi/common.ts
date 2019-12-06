826a697da8e98c65645c572276de45c06efc61b4
Garrett Serack <garretts@microsoft.com>
2019-12-06 08:42:30 -0800

    retain information if  was used in dereferencing

--- openapi/common.ts
+++ openapi/common.ts
@@ -30,6 +30,7 @@ export interface PathReference<T> {
 export interface Dereferenced<T> {
   instance: T;
   name: string;
+  fromRef?: boolean;
 }
 
 export type Reference<T> = T;
@@ -76,9 +77,9 @@ export function dereference<T>(document: any, item: Refable<T>, stack = new Arra
       // it's a ref to a ref.
       return dereference(document, node, stack);
     }
-    return { instance: node, name: name || '' };
+    return { instance: node, name: name || '', fromRef: true };
   }
-  return { instance: item, name: '' };
+  return { instance: item, name: '', fromRef: false };
 }
 
 
