c38f5a4cdc4f18cb1f8d8dee3837bd2f64701a0f
Garrett Serack <garretts@microsoft.com>
2020-02-07 11:13:10 -0800

    new impl of safe-eval that reuses the sandbox. much faster

--- codegen/safe-eval.ts
+++ codegen/safe-eval.ts
@@ -2,6 +2,28 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import * as vm from 'vm';
 
 /* eslint-disable */
-export const safeEval: <T>(expression: string, context?: any) => T = require('safe-eval');
+
+const sandbox = vm.createContext({});
+
+export function safeEval<T>(code: string, context?: any, opts?: any): T {
+  var resultKey = 'SAFE_EVAL_' + Math.floor(Math.random() * 1000000);
+  sandbox[resultKey] = {};
+  code = resultKey + '=' + code;
+
+  if (context) {
+    const keys = Object.keys(context);
+    keys.forEach(function (key) {
+      sandbox[key] = context[key];
+    })
+    vm.runInContext(code, sandbox, opts)
+    keys.forEach(function (key) {
+      delete sandbox[key];
+    })
+  } else {
+    vm.runInContext(code, sandbox, opts);
+  }
+  return sandbox[resultKey];
+}
