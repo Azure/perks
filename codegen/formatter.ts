83c4e84a25958642b1f53efda04f7945f4e0111b
Garrett Serack <garretts@microsoft.com>
2020-02-11 11:45:22 -0800

    add support for prefix and postfix on naming

--- codegen/formatter.ts
+++ codegen/formatter.ts
@@ -1,6 +1,7 @@
 import { fixLeadingNumber, isEqual, removeSequentialDuplicates } from './text-manipulation';
 import { Dictionary, values } from '@azure-tools/linq';
 
+
 export type Styler = ((identifier: string | Array<string>, removeDuplicates: boolean | undefined, overrides: Dictionary<string> | undefined) => string);
 
 function deconstruct(identifier: string | Array<string>): Array<string> {
@@ -14,6 +15,14 @@ function deconstruct(identifier: string | Array<string>): Array<string> {
     split(/[\W|_]+/).map(each => each.toLowerCase());
 }
 
+function wrap(prefix: string, postfix: string, style: Styler): Styler {
+  if (postfix || prefix) {
+    return (i, r, o) => typeof i === 'string' && typeof (o) === 'object' ? o[i.toLowerCase()] || `${prefix}${style(i, r, o)}${postfix}` : `${prefix}${style(i, r, o)}${postfix}`;
+  }
+  return style;
+}
+
+
 function applyFormat(normalizedContent: Array<string>, overrides: Dictionary<string> = {}, separator = '', formatter: (s: string, i: number) => string = (s, i) => s) {
   return normalizedContent.map((each, index) => overrides[each.toLowerCase()] || formatter(each, index)).join(separator);
 }
@@ -26,25 +35,34 @@ function normalize(identifier: string | Array<string>, removeDuplicates = true, 
 }
 export class Style {
   static select(style: any, fallback: Styler): Styler {
-    switch (`${style}`.toLowerCase()) {
-      case 'camelcase':
-      case 'camel':
-        return Style.camel;
-      case 'pascalcase':
-      case 'pascal':
-        return Style.pascal;
-      case 'snakecase':
-      case 'snake':
-        return Style.snake;
-      case 'uppercase':
-      case 'upper':
-        return Style.upper;
-      case 'kebabcase':
-      case 'kebab':
-        return Style.kebab;
-      case 'spacecase':
-      case 'space':
-        return Style.space;
+    if (style) {
+      const styles = /^([a-zA-Z0-9_]*?\+?)([a-zA-Z]+)(\+?[a-zA-Z0-9_]*)$/g.exec(style.replace(/\s*/g, ''));
+      if (styles) {
+        const prefix = styles[1] ? styles[1].substring(0, styles[1].length - 1) : '';
+        const postfix = styles[3] ? styles[3].substring(1) : '';
+
+        switch (styles[2]) {
+          case 'camelcase':
+          case 'camel':
+
+            return wrap(prefix, postfix, Style.camel);
+          case 'pascalcase':
+          case 'pascal':
+            return wrap(prefix, postfix, Style.pascal);
+          case 'snakecase':
+          case 'snake':
+            return wrap(prefix, postfix, Style.snake);
+          case 'uppercase':
+          case 'upper':
+            return wrap(prefix, postfix, Style.upper);
+          case 'kebabcase':
+          case 'kebab':
+            return wrap(prefix, postfix, Style.kebab);
+          case 'spacecase':
+          case 'space':
+            return wrap(prefix, postfix, Style.space);
+        }
+      }
     }
     return fallback;
   }
@@ -71,6 +89,4 @@ export class Style {
   static camel(identifier: string | Array<string>, removeDuplicates = true, overrides: Dictionary<string> = {}): string {
     return overrides[<string>identifier] || applyFormat(normalize(identifier, removeDuplicates), overrides, '', (each, index) => index ? each.capitalize() : each.uncapitalize());
   }
-
 }
-
