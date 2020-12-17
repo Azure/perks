5d0aaeb9495eae5e916e1e5c98237f1893cbb31e
Timothee Guerin <tiguerin@microsoft.com>
2020-12-17 16:41:16 +0100

    Revert to default to yarn but provide option to switch

--- extension/src/npm.ts
+++ extension/src/npm.ts
@@ -1,13 +1,15 @@
-import { ChildProcess, spawn, SpawnOptions } from "child_process";
 import { resolve } from "path";
+import { execute } from "./exec-cmd";
+import { InstallOptions, PackageManager } from "./package-manager";
 
-const DEFAULT_NPM_REGISTRY = "https://registry.npmjs.org";
+export const DEFAULT_NPM_REGISTRY = "https://registry.npmjs.org";
 
 const getPathToNpmCli = () => {
-  return resolve(`${__dirname}/../../node_modules/npm/bin/npm-cli.js`);
+  const npmPath = require.resolve("npm");
+  return resolve(`${npmPath}/bin/npm-cli.js`);
 };
 
-export const npm = async (cwd: string, ...args: string[]) => {
+export const execNpm = async (cwd: string, ...args: string[]) => {
   const procArgs = [
     getPathToNpmCli(),
     "--no-shrinkwrap",
@@ -18,46 +20,41 @@ export const npm = async (cwd: string, ...args: string[]) => {
   return await execute(process.execPath, procArgs, { cwd });
 };
 
-interface MoreOptions extends SpawnOptions {
-  onCreate?(cp: ChildProcess): void;
-  onStdOutData?(chunk: any): void;
-  onStdErrData?(chunk: any): void;
-}
-
-const execute = (
-  command: string,
-  cmdlineargs: Array<string>,
-  options: MoreOptions
-): Promise<{
-  stdout: string;
-  stderr: string;
-  error: Error | null;
-  code: number;
-}> => {
-  return new Promise((r, j) => {
-    const cp = spawn(command, cmdlineargs, { ...options, stdio: "pipe" });
-    if (options.onCreate) {
-      options.onCreate(cp);
-    }
-
-    options.onStdOutData ? cp.stdout.on("data", options.onStdOutData) : cp;
-    options.onStdErrData ? cp.stderr.on("data", options.onStdErrData) : cp;
-
-    let err = "";
-    let out = "";
-    cp.stderr.on("data", (chunk) => {
-      err += chunk;
-    });
-    cp.stdout.on("data", (chunk) => {
-      out += chunk;
-    });
-    cp.on("close", (code, signal) =>
-      r({
-        stdout: out,
-        stderr: err,
-        error: code ? new Error("Process Failed.") : null,
-        code,
-      })
+export class Npm implements PackageManager {
+  public async install(
+    directory: string,
+    packages: string[],
+    options?: InstallOptions
+  ) {
+    const output = await execNpm(
+      directory,
+      "install",
+      "--save",
+      "--prefix",
+      directory.replace(/\\/g, "/"),
+      ...(options?.force ? ["--force"] : []),
+      ...packages
     );
-  });
-};
+    if (output.error) {
+      throw Error(`Failed to install package '${packages}' -- ${output.error}`);
+    }
+  }
+
+  public async clean(directory: string): Promise<void> {
+    await execNpm(directory, "cache", "clean", "--force");
+  }
+
+  public async getPackageVersions(
+    directory: string,
+    packageName: string
+  ): Promise<string[]> {
+    const result = await execNpm(
+      directory,
+      "view",
+      packageName,
+      "versions",
+      "--json"
+    );
+    return JSON.parse(result.stdout).data;
+  }
+}
