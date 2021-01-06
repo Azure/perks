726beeec8632c2acd4a6d518c3e97fcf2c1347c7
Timothee Guerin <tiguerin@microsoft.com>
2021-01-06 18:19:47 +0100

    Added tests

--- codegen/test/formatter.test.ts
+++ codegen/test/formatter.test.ts
@@ -1,5 +1,26 @@
+import { Style } from "../formatter";
+
 describe("Formatter", () => {
-  it("Works", () => {
-    expect(true).toEqual(true);
+  describe("Snake case", () => {
+    it("convert camel case words", () => {
+      expect(Style.snake("fooBar")).toEqual("foo_bar");
+    });
+
+    it("convert word with spaces", () => {
+      expect(Style.snake("foo bar")).toEqual("foo_bar");
+    });
+
+    it("convert word with numbers", () => {
+      expect(Style.snake("foo123bar")).toEqual("foo123_bar");
+    });
+
+    it("convert words with upper cased words", () => {
+      expect(Style.snake("someSQLConnection")).toEqual("some_sql_connection");
+    });
+
+    it("it doesn't split upper case words with less than 2 characters", () => {
+      expect(Style.snake("diskMBpsReadWrite")).toEqual("disk_mbps_read_write");
+      expect(Style.snake("instanceIDs")).toEqual("instance_ids");
+    });
   });
 });
